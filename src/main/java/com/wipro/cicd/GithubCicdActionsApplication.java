4fa2b73e1295c0f70e86a2dde36fa91823623f5c
Shashank M K <mkshashanklcw@gmail.com>
2025-11-26 12:27:32 +0530

    code

---
+++ src/main/java/com/wipro/cicd/GithubCicdActionsApplication.java
@@ -0,0 +1,30 @@
+package com.wipro.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication
+{
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "Hello, Welcome to ci/cd!";
+    }
+
+	public static void main(String[] args)
+    {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
+//echo "# github-actions-example" >> README.md
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/mkshashank/github-actions-example.git
+//git push -u origin main
